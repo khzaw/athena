33c47da0dfb0d45a5abd55f358e367afa0167cd6
Kaung Htet Zaw <emoosx@gmail.com>
2014-06-14 18:25:25 +0800

    Properly adhere to module pattern

--- js/athena.js
+++ js/athena.js
@@ -1,65 +1,63 @@
-(function () {
+var athena = (function() {
+  var ORNAGAI = "http://www.ornagai.com/index.php/api/word/q/";
 
-  var dictionary = {
-
-    _makeRequest: function(url, success, error) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url);
-      xhr.addEventListener('load', function(e) {
-        if(xhr.status == 200) {
-          success(xhr.status, xhr.responseText);
-        } else {
-          if(error) {
-            error(xhr.status, xhr);
-          }
+  var makeRequest = function(word, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', ORNAGAI + word);
+    xhr.addEventListener('load', function(e) {
+      if(xhr.status == 200) {
+        success(xhr.status, xhr.responseText);
+      } else {
+        if(error) {
+          error(xhr.status, xhr);
         }
+      }
+    });
+    xhr.send();
+  };
+
+  var inOrnagai = function(word) {
+    var result = [];
+    makeRequest(word, function(status, response) {
+      var words = JSON.parse(response);
+      result = words.filter(function(value) {
+        return value.word.toLowerCase() === word.toLowerCase();
       });
-      xhr.send();
-    },
+      console.log("Inside: ", result);
+    });
+    console.log("Outside: ", result);
+    return result;
+  };
 
-    _inOrnagai: function(word) {
-      var ornagai = "http://www.ornagai.com/index.php/api/word/q/" + word;
-      var result = [];
-      this._makeRequest(ornagai, function(status, response) {
-        var words = JSON.parse(response);
-        result = words.filter(function(value) {
-          return value.word.toLowerCase() === word.toLowerCase();
-        });
-      });
-      console.log("Result", result);
-      return result;
-    },
+  var inOxford = function(word) {
+    console.log("looking up " + word + " in Oxford");
+  };
 
-    _inOxford: function(word) {
-      console.log("looking up " + word + " in oxford");
-    },
-
-    _render: function(myanmar, eng) {
-      console.log("rendering");
-    },
-
+  return {
     lookUp: function(word) {
-      var myanmar = this._inOrnagai(word);
-      var eng = this._inOxford(word);
-      this._render(myanmar, eng);
+      var myanmar = inOrnagai(word);
+      console.log("Myanmar", myanmar);
+
+      // inOxford(word);
     }
   };
 
-  document.addEventListener('dblclick', function(e) {
-    var focused = document.activeElement;
-    var selectedText;
-    if(focused) {
-      try {
-        selectedText = focused.value.substring(
-          focused.selectionStart, focused.selectionEnd);
-      } catch(err) {
-      }
-    }
-    if(selectedText === undefined) {
-      selectedText = window.getSelection().toString();
-    }
-
-    dictionary.lookUp(selectedText);
-  });
-
 })();
+
+document.addEventListener('dblclick', function(e) {
+  var focused = document.activeElement;
+  var selectedText;
+  if(focused) {
+    try {
+      selectedText = focused.value.substring(
+        focused.selectionStart, focused.selectionEnd);
+    } catch(err) {
+    }
+  }
+  if(selectedText === undefined) {
+    selectedText = window.getSelection().toString();
+  }
+
+  athena.lookUp(selectedText);
+
+});
